16ff2b23c57effdf35ad17ad6b24e979003a4004
wy333 <1577653650@qq.com>
2019-08-29 11:02:48 +0800

    测试react打包

---
+++ pack/react-app/precache-manifest.fd08e8d5557dcf7fe912d334dfc6df59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b185b1e2ecb25bf37cf9",
+    "url": "/static/js/main.d73e53ba.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "b185b1e2ecb25bf37cf9",
+    "url": "/static/css/main.a6d0f5b2.chunk.css"
+  },
+  {
+    "revision": "c84953045108b9b4a4e28a2c5bf66c24",
+    "url": "/index.html"
+  }
+];
